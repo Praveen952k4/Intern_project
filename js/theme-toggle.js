15df41a800692be27379e8f0e446593ec391a8c2
ilakkiya-22 <kannukiniyazhilak@gmail.com>
2025-06-24 15:09:12 +0530

    Show uploaded file name

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,6 +1,18 @@
 function switchTheme() {
-  const html = document.documentElement;
-  const knob = document.getElementById("theme-knob");
-  html.classList.toggle("dark");
-  knob.classList.toggle("translate-x-5");
-}
+    const html = document.documentElement;
+    const knob = document.getElementById("theme-knob");
+    html.classList.toggle("dark");
+    knob.classList.toggle("translate-x-5");
+  }
+
+  // File upload name change
+  document.addEventListener("DOMContentLoaded", function () {
+    const fileInput = document.getElementById("sourceFile");
+    const uploadText = document.getElementById("upload-text");
+
+    fileInput.addEventListener("change", function () {
+      if (fileInput.files.length > 0) {
+        uploadText.textContent = fileInput.files[0].name;
+      }
+    });
+  });
